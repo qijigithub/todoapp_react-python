ee224fbd388e0f13a288ae52e4e24d1c21799653
qijigithub <qijilearn@gmail.com>
2021-03-20 13:38:10 -0500

    run flask

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify,request,json
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_folder="build/static", template_folder="build")
+app = Flask(__name__, static_folder="../build/static", template_folder="../build")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
 db =SQLAlchemy(app)
 
